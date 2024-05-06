daf54aa63004676c654c0573771df788405b6b71
fabio almeida <f.almeida.work@gmail>
2024-05-06 14:20:59 +0100

    Added multiline in chat

--- chat.py
+++ chat.py
@@ -29,6 +29,8 @@ class Chat(Events):
         self.max_chat_log = 30
         self.cache_messages = True
         self.current_prompt = ""
+        self._is_multiline_input = False
+        self._multiline_input = ""
 
     def _add_message(self, role, message):
         if self.cache_messages:
@@ -46,6 +48,28 @@ class Chat(Events):
         while not self.terminate:
             self.process_loop_frame()    
 
+    def check_and_handle_user_input_multiline(self, user_input:str):
+        
+        if user_input is not None and len(user_input):
+            multiline_start = user_input.strip().startswith('"""') 
+            multiline_end = user_input.strip().endswith('"""')
+             
+            if self._is_multiline_input:
+                self._multiline_input +=  user_input
+                if  multiline_end:
+                    self._multiline_input += user_input.strip()[:-3]
+                    self.send_chat(self._multiline_input)
+                    self._multiline_input = ""
+                    self._is_multiline_input = False
+                else:
+                    self._multiline_input += user_input+"\n"
+                return True
+            elif multiline_start:
+                self._multiline_input += user_input.strip()[2:]
+                self._is_multiline_input = True
+                return True         
+        return False
+    
     def process_loop_frame(self):
         """ This function runs a frame in the chat loop """
         if self.running_command:
@@ -54,8 +78,11 @@ class Chat(Events):
             return  # Skip the rest of the loop and go to next iteration
         elif self.waiting_for_response:
             return
+        
+        elif self._is_multiline_input:
+            user_input = input("... ")
         else:
-            print(format_text(self.user_prompt,Color.BLUE) , end=" ",flush=True)
+            print(format_text(self.user_prompt, Color.BLUE) , end=" ",flush=True)
             user_input = input()
 
         if len(user_input.strip()) == 0:
@@ -67,7 +94,8 @@ class Chat(Events):
         elif user_input.lower() in self.terminate_tokens:
             self.terminate_chat()
         else:
-            self.send_chat(user_input)    
+            if not self.check_and_handle_user_input_multiline(user_input):
+                self.send_chat(user_input)    
 
     def send_chat(self, message):
         self.waiting_for_response = True
