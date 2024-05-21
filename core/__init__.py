da124a70a5cd5430d66df13a34d7c2d808f2a981
Fabio Almeida <f.almeida.work@gmail.com>
2024-05-21 09:59:36 +0100

    temp commit

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,6 @@
-from ai.core.chat import Chat, ChatRoles
-from ai.core.chat_command_interceptor import ChatCommandInterceptor
-from ai.core.events import Events
-from ai.core.llm import OllamaModel
-from ai.core.command_executor import AsyncExecutor,CommandExecutor,ExecutorResult
-from ai.core.context_file import ContextFile
+from core.chat import Chat, ChatRoles
+from core.chat_command_interceptor import ChatCommandInterceptor
+from core.events import Events
+from core.llm import OllamaModel
+from core.command_executor import AsyncExecutor,CommandExecutor,ExecutorResult
+from core.context_file import ContextFile
